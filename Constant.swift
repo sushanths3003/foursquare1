299bcbf375a7c5af6a86a8a752af31902d9e972d
bhoomikaHPgowda <72902172+bhoomikaHPgowda@users.noreply.github.com>
2021-06-25 23:26:14 +0530

    Add files via upload

---
+++ Constant.swift
@@ -0,0 +1,24 @@
+//
+//  Constant.swift
+//  Wheather
+//
+//  Created by Bhoomika H P on 10/06/21.
+//
+
+import Foundation
+
+enum WeatherCondition: String {
+    case minmax = "Min-Max"
+    case precipitation = "Precipitation "
+    case humudity = "Humidity"
+    case wind = "Wind"
+    case visibality = "Visibality"
+}
+
+enum WeatherImages: String {
+    case temperature = "temperature"
+    case rain = "rain"
+    case humidity = "humidity"
+    case wind = "wind"
+    case visibality = "visibality"
+}
